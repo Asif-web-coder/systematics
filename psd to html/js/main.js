a0d84a296c396d8556f61aa6e6ec37c6c58b40bb
ASIFUL ISLAM <76622859+Asif-web-coder@users.noreply.github.com>
2021-03-17 21:37:31 +0600

    systematics psd to html
    I Created PSD to HTML. here is the all source code.

---
+++ psd to html/js/main.js
@@ -0,0 +1,13 @@
+// Bx slider
+$(document).ready(function(){
+    $('.slider_area').bxSlider();
+  });
+
+ 
+  $(document).ready(function(){
+    $('.main_manu').meanmenu({
+      meanMenuContainer: '#mobile_menu',
+      meanScreenWidth: '991',
+    });
+    });
+  
